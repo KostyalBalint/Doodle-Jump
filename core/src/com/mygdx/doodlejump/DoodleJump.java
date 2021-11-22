1cd35dafcd8bb904f061af6a7f8cf629e33dc237
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-11-22 19:45:50 +0100

    wip

--- core/src/com/mygdx/doodlejump/DoodleJump.java
+++ core/src/com/mygdx/doodlejump/DoodleJump.java
@@ -1,31 +1,58 @@
 package com.mygdx.doodlejump;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.List;
+
 public class DoodleJump extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private OrthographicCamera camera;
+
+	private Doodler doodler;
+
+	private List<Platform> platforms;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		//Clear the screen
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		//Update the camera
+		camera.update();
+
+		//Draw all the platforms
+		platforms.forEach(platform -> platform.draw(camera));
+
+		//Draw the black hole if any
+
+		//Draw the doodler
+
+		//Process user input
+		if(Gdx.input.isKeyPressed(Keys.LEFT)){
+			//Move the doodler left
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+			//Move the doodler right
+		}
+
+		//Update camera position
+		//Camera is based on the highest level, that the doodler jumped on
+
+		//Handle doodler auto-jump
+
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		//Dispose all the resources
 	}
 }
--- desktop/src/com/mygdx/doodlejump/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/doodlejump/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.doodlejump.DoodleJump;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Doodle Jump";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new DoodleJump(), config);
 	}
 }
